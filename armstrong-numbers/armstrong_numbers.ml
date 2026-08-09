2c7692ca56fe7474207a9dabd4f61fee0ef022cc
EternallyDebuggingKnight <j.paul.vodeme@proton.me>
2026-08-09 15:31:14 +0000

    Add solution to armstrong number

---
+++ armstrong-numbers/armstrong_numbers.ml
@@ -0,0 +1,35 @@
+
+let pow  x y =
+      let rec mul_n_times x y z =
+      if (y == 0) then
+        1
+      else if (y == 1) then
+      x
+      else
+        mul_n_times (x*z) (y - 1) z
+    in mul_n_times x y x
+
+let len_of_num num = int_of_float ( log10 (float_of_int num) +. 1.0)
+
+let validate candidate = (
+    if (candidate < 10) then
+    true
+    else if (candidate < 100) then 
+    false
+    else
+      let len = len_of_num candidate in
+      let digit_sum =
+        let rec reduce_num acc num = (
+        (* Printf.printf "* acc = %d,\n* num = %d\n" acc num; *)
+         let digit = num mod 10 in
+         match num with
+         | 0 -> acc
+         | _ -> reduce_num (acc + (pow  digit len)) (num / 10)
+        )  in reduce_num 0 candidate
+    in
+    (* Printf.printf "digit_sum = %d\n" digit_sum; *)
+    if (digit_sum = candidate) then
+      true
+    else
+      false
+  )
