8f2eb83cfca9226d802a5e88acc85d6b85d316ec
EternallyDebuggingKnight <j.paul.vodeme@proton.me>
2026-08-09 16:11:49 +0000

    Add solution to 'darts'

--- darts/darts.ml
+++ darts/darts.ml
@@ -1,2 +1,7 @@
-let score (x: float) (y: float): int =
-  failwith "Please implement 'score'"
+let score (x: float) (y: float): int = 
+  let d = sqrt (x ** 2.0 +. y ** 2.0 ) in
+  match d with 
+    | _ when d > 10.0 -> 0
+    | _ when d > 5.0 -> 1
+    | _ when d > 1.0 -> 5
+    | _ -> 10
